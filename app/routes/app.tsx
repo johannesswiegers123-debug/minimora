012f873cd3a4d08b491f6235883242a85dbdb74b
Johannes Swiegers <johannes@minimora.com>
2025-12-18 22:52:15 +0100

    Fix NavMenu import from correct package

--- app/routes/app.tsx
+++ app/routes/app.tsx
@@ -1,7 +1,8 @@
 import type { LoaderFunctionArgs } from "react-router";
 import { Outlet, useLoaderData, useRouteError } from "react-router";
 import { boundary } from "@shopify/shopify-app-react-router/server";
-import { AppProvider, NavMenu } from "@shopify/shopify-app-react-router/react";
+import { AppProvider } from "@shopify/shopify-app-react-router/react";
+import { NavMenu } from "@shopify/app-bridge-react";
 
 import { authenticate } from "../shopify.server";
 
